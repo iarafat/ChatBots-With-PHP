b52d6a35455778aabe5bb9bf05b99da18c9ca72d
iarafat <miyasinarafat@gmail.com>
2019-03-04 11:23:12 +0600

    added user reply response from bot for telegram

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
+
 use Monolog\Logger;
 use \Monolog\Handler\StreamHandler;
-require_once  __DIR__.'/vendor/autoload.php';
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+// create log file and save the log and save response from user.
 try {
     $log = new Logger('general');
     $log->pushHandler(new StreamHandler('debug.log'));
@@ -12,9 +15,58 @@ try {
     $dotenv->load();
     //$log->info('Token: '.getenv('TELEGRAM_TOKEN'));
 
-    $input = json_decode(file_get_contents('php://input'), true) ?? [];
-    $log->info('Incoming request: ', $input);
+    // save response from user
+   // $input = json_decode(file_get_contents('php://input'), true) ?? [];
+    //$log->info('Incoming request: ', $input);
 
 } catch (Exception $e) {
     var_dump($e->getMessage());
 }
+
+// send response to user.
+$telegramReply = [
+    'chat_id' => '668849417',
+    'text' => 'This is a reply',
+];
+$url = 'https://api.telegram.org/bot'.getenv('TELEGRAM_TOKEN').'/sendmessage';
+
+$ch = curl_init($url);
+
+// tell curl to send post request.
+curl_setopt($ch, CURLOPT_POST, 1);
+
+// attach json string to post fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($telegramReply));
+
+// set the content type
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+// execute
+curl_exec($ch);
+if (curl_error($ch)) {
+    $log->warning('Curl error: ' . curl_error($ch));
+}
+curl_close($ch);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
