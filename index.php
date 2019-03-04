c0c3ef915fa81857b276505731b6f117b650d04b
iarafat <miyasinarafat@gmail.com>
2019-03-04 12:45:31 +0600

    completed first facebook chatbot

--- index.php
+++ index.php
@@ -23,34 +23,45 @@ try {
     var_dump($e->getMessage());
 }
 
+// verify webhooks
+/*if (isset($_REQUEST['hub_challenge'])){
+    $fbHubMode = $_GET['hub_mode'];
+    $fbHubVerifyToken = $_GET['hub_verify_token'];
+    $fbHubChallenge = $_GET['hub_challenge'];
+
+    if ($fbHubMode === 'subscribe' && $fbHubVerifyToken === getenv('FACEBOOK_VERIFY_TOKEN')){
+        $log->info('Inside verify method');
+        echo  $fbHubChallenge;
+    }
+    exit;
+}*/
+
 // send response to user.
-$telegramReply = [
-    'chat_id' => '668849417',
-    'text' => 'This is a reply',
+$facebookReply = [
+    'messaging_type' => 'RESPONSE',
+    'recipient' => [
+        'id' => '2580600762014365'
+    ],
+    'message' => [
+        'text' => 'This is a reply.'
+    ],
 ];
-$url = 'https://api.telegram.org/bot'.getenv('TELEGRAM_TOKEN').'/sendmessage';
+$url = 'https://graph.facebook.com/v3.0/me/messages?access_token='.getenv('FACEBOOK_PAGE_ACCESS_TOKEN');
+
 
 $ch = curl_init($url);
-
 // tell curl to send post request.
 curl_setopt($ch, CURLOPT_POST, 1);
-
 // attach json string to post fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($telegramReply));
-
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($facebookReply));
 // set the content type
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-
 // execute
 curl_exec($ch);
 if (curl_error($ch)) {
     $log->warning('Curl error: ' . curl_error($ch));
 }
 curl_close($ch);
-
-
-
-
 
 
 
